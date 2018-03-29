332ec81afd5c1ecd5a3ab66b626f786adb7ef1f2
Brandon Johnson <bmj1985@gmail.com>
2018-03-29 15:31:46 -0600

    routes with mobile friendly styling

---
+++ src/components/Access.vue
@@ -0,0 +1,95 @@
+<template>
+    <v-container class="access access-page">
+      <v-layout column align-center class="logo">
+        <h1><span>{{title_span1}}</span> {{title}} <span>{{title_span2}}</span></h1>
+        <div class="text-xs-center tri-links">
+          <router-link class="nav-link" :to="{ name: 'Roster'}">
+            <v-btn round color="blue" large dark>
+              Roster
+            </v-btn>
+          </router-link>
+          <router-link class="nav-link" :to="{ name: 'Trail'}">
+            <v-btn round color="green" large dark>
+              Trail Runners
+            </v-btn>
+          </router-link>
+          <router-link class="nav-link" :to="{ name: 'AidStation'}">
+            <v-btn round color="taupe" large dark>
+              Aid-Station
+            </v-btn>
+          </router-link>
+        </div>
+        <div class="text-xs-center home-link">
+          <router-link class="nav-link-home" :to="{ name: 'SplashPage'}">
+            <v-btn round color="secondary" class="btn-home" large dark>
+              Home
+            </v-btn>
+          </router-link>
+        </div>
+      </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Access',
+  data () {
+    return {
+      title: 'LONESOME',
+      title_span1: 'HIGH',
+      title_span2: '100'
+    }
+  },
+  mounted () {
+    // this.getDataFromDatabase()
+  },
+  methods: {
+    // getDataFromDatabase () {
+    //   fetch(this.apiUrl)
+    //     .then(response => response.json())
+    //     .then(response => {
+    //       // console.log(response)
+    //       this.runners = response
+    //     })
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.access-page {
+  height: 700px;
+}
+
+span {
+  font-weight: 400;
+}
+.tri-links {
+  margin-top: 100px;
+}
+.tri-links button {
+  margin-bottom: 30px;
+}
+.home-link {
+ margin-top: 140px;
+}
+.nav-link .btn {
+  color: #303131;
+  font-size: 2rem;
+  height: 5rem;
+  width: 25rem;
+}
+.nav-link {
+  text-decoration: none;
+}
+.nav-link-home {
+  text-decoration: none;
+}
+.nav-link-home .btn-home {
+  color: #A97C54;
+  font-size: 2rem;
+  height: 5rem;
+  width: 15rem;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,46 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import SplashPage from '@/components/SplashPage'
-import RunnerCheckIn from '@/components/RunnerCheckIn'
+import Vue from 'vue';
+import Router from 'vue-router';
+import SplashPage from '@/components/SplashPage';
+import RunnerCheckIn from '@/components/RunnerCheckIn';
+import Access from '@/components/Access';
+import Roster from '@/components/Roster';
+import Trail from '@/components/Trail';
+import AidStation from '@/components/AidStation';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'SplashPage',
-      component: SplashPage
+      component: SplashPage,
     },
     {
       path: '/runnercheckin',
       name: 'RunnerCheckIn',
-      component: RunnerCheckIn
-    }
-  ]
-})
+      component: RunnerCheckIn,
+    },
+    {
+      path: '/access',
+      name: 'Access',
+      component: Access,
+    },
+    {
+      path: '/roster',
+      name: 'Roster',
+      component: Roster,
+    },
+    {
+      path: '/trail',
+      name: 'Trail',
+      component: Trail,
+    },
+    {
+      path: '/aid-station',
+      name: 'AidStation',
+      component: AidStation,
+    },
+  ],
+});
