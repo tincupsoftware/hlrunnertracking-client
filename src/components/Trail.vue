42cd5414425ba4bc3db772cc9d26d88ede679d41
Charles Ballew <charleswballewjr@gmail.com>
2018-03-30 12:15:25 -0600

    roster success and raido btn done

--- src/components/Trail.vue
+++ src/components/Trail.vue
@@ -1,30 +1,30 @@
 <template>
-    <v-container class="trail">
-      <v-layout column align-center class="logo">
-        <h1><span>{{title_span1}}</span> {{title}} <span>{{title_span2}}</span></h1>
-        <div class="text-xs-center trail-link">
-          <router-link class="nav-link-trail" :to="{ name: 'Access'}">
-            <v-btn round color="secondary" class="btn-trail" large dark>
-              BACK
-            </v-btn>
-          </router-link>
-            <v-btn round color="secondary" class="btn-trail" large dark>
-              add
-            </v-btn>
-        </div>
-        <RunnerList/>
-      </v-layout>
-    </v-container>
+<v-container class="trail">
+  <v-layout column align-center class="logo">
+    <h1><span>{{title_span1}}</span> {{title}} <span>{{title_span2}}</span></h1>
+    <div class="text-xs-center trail-link">
+      <router-link class="nav-link-trail" :to="{ name: 'Access'}">
+        <v-btn round color="secondary" class="btn-trail" large dark>
+          BACK
+        </v-btn>
+      </router-link>
+      <v-btn round color="secondary" class="btn-trail" large dark>
+        add
+      </v-btn>
+    </div>
+    <RunnerList/>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
 import RunnerList from '@/components/RunnerList'
 export default {
   name: 'Trail',
-  components: { 
-    RunnerList 
-    },
-    data () {
+  components: {
+    RunnerList
+  },
+  data () {
     return {
       title: 'LONESOME',
       title_span1: 'HIGH',
@@ -38,15 +38,18 @@ export default {
 span {
   font-weight: 400;
 }
+
 .btn {
   font-size: 2rem;
   height: 5rem;
   width: 25rem;
 }
+
 .nav-link {
   color: #303131;
   text-decoration: none;
 }
+
 .nav-link-trail .btn-trail {
   color: #A97C54;
   font-size: 2rem;
@@ -54,6 +57,7 @@ span {
   width: 10rem;
   margin-bottom: 25px;
 }
+
 .nav-link-trail {
   text-decoration: none;
 }
