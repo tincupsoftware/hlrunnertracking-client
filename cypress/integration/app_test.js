44c1b71768e2bbf83c893407763bf5d3af343170
Brandon Johnson <bmj1985@gmail.com>
2018-04-01 13:53:55 -0600

    using cypress to add runners to db. will it work?

--- cypress/integration/app_test.js
+++ cypress/integration/app_test.js
@@ -1,14 +1,513 @@
-describe('hl100 runner tracking', function () {
-    it('shows correct content', function () {
-        cy.visit('https://highlonesome100runnertracking.firebaseapp.com');
+const runners = [
+  {
+    bibNumber: 10,
+    firstName: 'Ryan',
+    lastName: 'Smith'
+  },
+  {
+    bibNumber: 11,
+    firstName: 'Anthony',
+    lastName: 'Lee'
+  },
+  {
+    bibNumber: 12,
+    firstName: 'John',
+    lastName: 'Wros'
+  },
+  {
+    bibNumber: 13,
+    firstName: 'Ryan',
+    lastName: 'Case'
+  },
+  {
+    bibNumber: 14,
+    firstName: 'Ernest',
+    lastName: 'Lamaitis'
+  },
+  {
+    bibNumber: 15,
+    firstName: 'Tom',
+    lastName: 'Steidler'
+  },
+  {
+    bibNumber: 16,
+    firstName: 'Heather',
+    lastName: 'Jossi'
+  },
+  {
+    bibNumber: 17,
+    firstName: 'Brian',
+    lastName: 'Crone'
+  },
+  {
+    bibNumber: 18,
+    firstName: 'Cory',
+    lastName: 'Linfield'
+  },
+  {
+    bibNumber: 19,
+    firstName: 'Cortland',
+    lastName: 'Wheeler'
+  },
+  {
+    bibNumber: 20,
+    firstName: 'Austin',
+    lastName: 'Black'
+  },
+  {
+    bibNumber: 21,
+    firstName: 'Scott',
+    lastName: 'Muggleton'
+  },
+  {
+    bibNumber: 22,
+    firstName: 'Dave',
+    lastName: 'Scheibel'
+  },
+  {
+    bibNumber: 23,
+    firstName: 'Jason',
+    lastName: 'Halladay'
+  },
+  {
+    bibNumber: 24,
+    firstName: 'David',
+    lastName: 'Coblentz'
+  },
+  {
+    bibNumber: 25,
+    firstName: 'Jeff',
+    lastName: 'Wanner'
+  },
+  {
+    bibNumber: 26,
+    firstName: 'Scott',
+    lastName: 'Ownbey'
+  },
+  {
+    bibNumber: 27,
+    firstName: 'David',
+    lastName: 'Galloway'
+  },
+  {
+    bibNumber: 28,
+    firstName: 'Jeff',
+    lastName: 'Bertot'
+  },
+  {
+    bibNumber: 29,
+    firstName: 'Ryan',
+    lastName: 'Lassen'
+  },
+  {
+    bibNumber: 30,
+    firstName: 'Arya',
+    lastName: 'Farahani'
+  },
+  {
+    bibNumber: 31,
+    firstName: 'Ben',
+    lastName: 'Woodbeck'
+  },
+  {
+    bibNumber: 32,
+    firstName: 'Ethan',
+    lastName: 'Printy'
+  },
+  {
+    bibNumber: 33,
+    firstName: 'Kevin',
+    lastName: 'Cannaday'
+  },
+  {
+    bibNumber: 34,
+    firstName: 'Chad',
+    lastName: 'Allen'
+  },
+  {
+    bibNumber: 35,
+    firstName: 'Ken',
+    lastName: 'Gordon'
+  },
+  {
+    bibNumber: 36,
+    firstName: 'Brennen',
+    lastName: 'Wysong'
+  },
+  {
+    bibNumber: 37,
+    firstName: 'Dave',
+    lastName: 'Herring'
+  },
+  {
+    bibNumber: 38,
+    firstName: 'Mike',
+    lastName: 'Hoog'
+  },
+  {
+    bibNumber: 39,
+    firstName: 'Billy',
+    lastName: 'Grimes'
+  },
+  {
+    bibNumber: 40,
+    firstName: 'John',
+    lastName: 'Stanfield'
+  },
+  {
+    bibNumber: 41,
+    firstName: 'Andrew',
+    lastName: 'Rivers'
+  },
+  {
+    bibNumber: 42,
+    firstName: 'Walter',
+    lastName: 'Handloser'
+  },
+  {
+    bibNumber: 43,
+    firstName: 'Kerrie',
+    lastName: 'Bruxvoort'
+  },
+  {
+    bibNumber: 44,
+    firstName: 'Tim',
+    lastName: 'Laney'
+  },
+  {
+    bibNumber: 45,
+    firstName: 'Jon',
+    lastName: 'Webb'
+  },
+  {
+    bibNumber: 46,
+    firstName: 'Charles',
+    lastName: 'Johnston'
+  },
+  {
+    bibNumber: 47,
+    firstName: 'Garth',
+    lastName: 'Reader'
+  },
+  {
+    bibNumber: 48,
+    firstName: 'Adam',
+    lastName: 'Chapman'
+  },
+  {
+    bibNumber: 49,
+    firstName: 'Nikki',
+    lastName: 'Larochelle'
+  },
+  {
+    bibNumber: 50,
+    firstName: 'Eric',
+    lastName: 'Moortgat'
+  },
+  {
+    bibNumber: 51,
+    firstName: 'Michael',
+    lastName: 'Hlavacek'
+  },
+  {
+    bibNumber: 52,
+    firstName: 'Will',
+    lastName: 'Jorgensen'
+  },
+  {
+    bibNumber: 53,
+    firstName: 'Chris',
+    lastName: 'Swedenborg'
+  },
+  {
+    bibNumber: 54,
+    firstName: 'Max',
+    lastName: 'Bohning'
+  },
+  {
+    bibNumber: 55,
+    firstName: 'Matt',
+    lastName: 'Ozanic'
+  },
+  {
+    bibNumber: 56,
+    firstName: 'Michaeline',
+    lastName: 'Nelson Albright'
+  },
+  {
+    bibNumber: 57,
+    firstName: 'John',
+    lastName: 'Danese'
+  },
+  {
+    bibNumber: 58,
+    firstName: 'Tommy',
+    lastName: 'Stockton'
+  },
+  {
+    bibNumber: 59,
+    firstName: 'Logan',
+    lastName: 'Troutman'
+  },
+  {
+    bibNumber: 60,
+    firstName: 'Paul',
+    lastName: 'Valentine'
+  },
+  {
+    bibNumber: 61,
+    firstName: 'Josh',
+    lastName: 'Myers-Dean'
+  },
+  {
+    bibNumber: 62,
+    firstName: 'Patrick',
+    lastName: 'Olson'
+  },
+  {
+    bibNumber: 63,
+    firstName: 'Colleen',
+    lastName: 'Powers'
+  },
+  {
+    bibNumber: 64,
+    firstName: 'Jorge',
+    lastName: 'Rasillo'
+  },
+  {
+    bibNumber: 65,
+    firstName: 'Jason',
+    lastName: 'Steidler'
+  },
+  {
+    bibNumber: 66,
+    firstName: 'Bard',
+    lastName: 'Parnell'
+  },
+  {
+    bibNumber: 67,
+    firstName: 'Ben',
+    lastName: 'Moritz'
+  },
+  {
+    bibNumber: 68,
+    firstName: 'Mike',
+    lastName: 'Smith'
+  },
+  {
+    bibNumber: 69,
+    firstName: 'Jon',
+    lastName: 'Davis'
+  },
+  {
+    bibNumber: 70,
+    firstName: 'Simon',
+    lastName: 'Roberts'
+  },
+  {
+    bibNumber: 71,
+    firstName: 'Martin',
+    lastName: 'Almazan'
+  },
+  {
+    bibNumber: 72,
+    firstName: 'Jessica',
+    lastName: 'Klabak'
+  },
+  {
+    bibNumber: 73,
+    firstName: 'Leora',
+    lastName: 'Wallace'
+  },
+  {
+    bibNumber: 74,
+    firstName: 'Tim',
+    lastName: 'White'
+  },
+  {
+    bibNumber: 75,
+    firstName: 'Geoff',
+    lastName: 'Moffat'
+  },
+  {
+    bibNumber: 76,
+    firstName: 'Philip',
+    lastName: 'Snyder'
+  },
+  {
+    bibNumber: 77,
+    firstName: 'Jennifer',
+    lastName: 'Thompson'
+  },
+  {
+    bibNumber: 78,
+    firstName: 'Elissa',
+    lastName: 'Price'
+  },
+  {
+    bibNumber: 79,
+    firstName: 'Rachel',
+    lastName: 'Brewer'
+  },
+  {
+    bibNumber: 80,
+    firstName: 'Robert',
+    lastName: 'Howard'
+  },
+  {
+    bibNumber: 81,
+    firstName: 'Alex',
+    lastName: 'Steidler'
+  },
+  {
+    bibNumber: 82,
+    firstName: 'Quinn',
+    lastName: 'Aaron'
+  },
+  {
+    bibNumber: 83,
+    firstName: 'Courtney',
+    lastName: 'Foley'
+  },
+  {
+    bibNumber: 84,
+    firstName: 'Matt',
+    lastName: 'Ross'
+  },
+  {
+    bibNumber: 85,
+    firstName: 'Jonathan',
+    lastName: 'Eisen'
+  },
+  {
+    bibNumber: 86,
+    firstName: 'Vincent',
+    lastName: 'Gauthier'
+  },
+  {
+    bibNumber: 87,
+    firstName: 'Sarah',
+    lastName: 'Turner'
+  },
+  {
+    bibNumber: 88,
+    firstName: 'Andrew',
+    lastName: 'Nichols'
+  },
+  {
+    bibNumber: 89,
+    firstName: 'Zach',
+    lastName: 'Przybeck'
+  },
+  {
+    bibNumber: 90,
+    firstName: 'Hannah',
+    lastName: 'Taylor'
+  },
+  {
+    bibNumber: 91,
+    firstName: 'Tyler',
+    lastName: 'Richter'
+  },
+  {
+    bibNumber: 92,
+    firstName: 'David',
+    lastName: 'Braun'
+  },
+  {
+    bibNumber: 93,
+    firstName: 'Peter',
+    lastName: 'Wing'
+  },
+  {
+    bibNumber: 94,
+    firstName: 'Jim',
+    lastName: 'Skelding'
+  },
+  {
+    bibNumber: 95,
+    firstName: 'Heather',
+    lastName: 'McComb'
+  },
+  {
+    bibNumber: 96,
+    firstName: 'Gracie',
+    lastName: 'Cole'
+  },
+  {
+    bibNumber: 97,
+    firstName: 'James',
+    lastName: 'Brynteson'
+  },
+  {
+    bibNumber: 98,
+    firstName: 'Wayne',
+    lastName: 'McComb'
+  },
+  {
+    bibNumber: 99,
+    firstName: 'Jena',
+    lastName: 'Miller'
+  },
+  {
+    bibNumber: 100,
+    firstName: 'Amanda',
+    lastName: 'Grimes'
+  },
+  {
+    bibNumber: 101,
+    firstName: 'Hannah',
+    lastName: 'Combs'
+  },
+  {
+    bibNumber: 102,
+    firstName: 'Laura',
+    lastName: 'OConnell'
+  },
+  {
+    bibNumber: 103,
+    firstName: 'Wanvipa',
+    lastName: 'Ruangpradit'
+  },
+  {
+    bibNumber: 104,
+    firstName: 'Anna',
+    lastName: 'Norlin'
+  },
+  {
+    bibNumber: 105,
+    firstName: 'Leigh-Ann',
+    lastName: 'Wendling'
+  },
+  {
+    bibNumber: 106,
+    firstName: 'Kristine',
+    lastName: 'Gillis'
+  },
+  {
+    bibNumber: 107,
+    firstName: 'Scott',
+    lastName: 'White'
+  },
+  {
+    bibNumber: 108,
+    firstName: 'Camilo',
+    lastName: 'Casas'
+  }
+]
 
-        cy.get('h1').should('have.text', 'High Lonesome 100');
+
+describe('hl100 runner tracking', function () {
+  it('shows correct content', function () {
+      cy.visit('https://highlonesome100runnertracking.firebaseapp.com')
+
+        cy.get('h1').should('have.text', 'High Lonesome 100')
 
         cy
             .get('div.btn__content')
             .click()
             .url()
-            .should('contain', '/access');
+            .should('contain', '/access')
 
         cy
             .get('div.text-xs-center.tri-links')
@@ -16,9 +515,9 @@ describe('hl100 runner tracking', function () {
             .eq(0)
             .click()
             .url()
-            .should('contain', '/roster');
+            .should('contain', '/roster')
 
-        cy.get('h1').should('contain', 'RUNNERS');
+        cy.get('h1').should('contain', 'RUNNERS')
 
         cy
             .get('div.text-xs-center.roster-link')
@@ -26,10 +525,11 @@ describe('hl100 runner tracking', function () {
             .eq(1)
             .click()
             .url()
-            .should('contain', '/runnercheckin');
+            .should('contain', '/runnercheckin')
 
-        cy.get('[aria-label="Bib #"]').type('35');
-        cy.get('[aria-label="Name"]').type('Morgan Johnson');
+        runners.map(runner => {
+            cy.get('[aria-label="Bib #"]').type(`${runner.bibNumber}`)
+        cy.get('[aria-label="Name"]').type(`${runner.firstName} ${runner.lastName}`)
         cy
             .get('div.container.fluid')
             .find('div.input-group.input-group--dirty.radio-group.radio-group--row')
@@ -39,19 +539,19 @@ describe('hl100 runner tracking', function () {
             .find('div.input-group.input-group--selection-controls.radio.accent--text')
             .eq(1)
             .find('div.input-group--selection-controls__ripple')
-            .click();
+            .click()
         cy
             .get('div.container.fluid')
             .find('div.input-group.input-group--dirty.radio-group.radio-group--row')
             .eq(1)
             .find('[aria-label="M"]')
             .find('div.input-group--selection-controls__ripple')
-            .click();
-        cy.get('[aria-label="Location Staying"]').type('Holiday INN');
+            .click()
+        cy.get('[aria-label="Location Staying"]').type('')
         cy
             .get('div.text-xs-center.roster-link')
             .find('button')
             .eq(0)
-            .click();
-    });
-});
+            .click()
+    })
+})
